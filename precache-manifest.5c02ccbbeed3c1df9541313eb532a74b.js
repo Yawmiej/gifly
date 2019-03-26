0b527d0f09df1d6b4ea821f1e83c3d4031030fd2
Abdullateef <yomlateef@yahoo.com>
2019-03-26 10:41:46 +0100

    Updates

---
+++ precache-manifest.5c02ccbbeed3c1df9541313eb532a74b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "45a01c85161e8f8ecd38ac7e58923468",
+    "url": "/gifly/static/media/background.45a01c85.jpg"
+  },
+  {
+    "revision": "da2f07165a28bce7a619",
+    "url": "/gifly/static/js/runtime~main.9da0dbd3.js"
+  },
+  {
+    "revision": "69a2b7374c38d378bd0f",
+    "url": "/gifly/static/js/main.1b549a1b.chunk.js"
+  },
+  {
+    "revision": "3f9d57edf47e9ed7c15d",
+    "url": "/gifly/static/js/2.8855a5ba.chunk.js"
+  },
+  {
+    "revision": "69a2b7374c38d378bd0f",
+    "url": "/gifly/static/css/main.ab3f6a0a.chunk.css"
+  },
+  {
+    "revision": "3f9d57edf47e9ed7c15d",
+    "url": "/gifly/static/css/2.5d3f5915.chunk.css"
+  },
+  {
+    "revision": "6b6817ce03abebff5ff46746b07151eb",
+    "url": "/gifly/index.html"
+  }
+];
